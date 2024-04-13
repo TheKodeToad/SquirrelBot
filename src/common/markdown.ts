08f5aceacf1beb8f4400c386cafec818bd7dbb6b
TheKodeToad <TheKodeToad@proton.me>
2024-04-13 17:09:39 +0100

    Fix markdown regex

--- src/common/markdown.ts
+++ src/common/markdown.ts
@@ -1,7 +1,7 @@
 // everything where an escape is valid and the character sometimes affects formatting
 // (all punctuation seems to be possible to escape?)
 // perhaps this is not the best way to do this but it should work everywhere apart from inside codeblocks
-const FORMATTING_REGEX = /[\\*_\-`#@<>.~|:]/;
+const FORMATTING_REGEX = /[\\/*_\-`#@<>.~|:\[\]\(\)]/g;
 
 export function escape_all(input: string) {
 	return input.replace(FORMATTING_REGEX, "\\$&");
