082a10723c42e9dd9eb1d4586b41d7c1f6f666b1
TheKodeToad <TheKodeToad@proton.me>
2024-05-04 22:44:32 +0100

    Remove ephemeral from Reply

--- src/core/types/command.ts
+++ src/core/types/command.ts
@@ -33,7 +33,7 @@ export interface Context {
 	respond(reply: Reply): Promise<void>;
 }
 
-export type Reply = Omit<CreateMessageOptions, "messageReference" | "tts"> & { ephemeral?: boolean; } | string;
+export type Reply = Omit<CreateMessageOptions, "messageReference" | "tts"> | string;
 
 export interface CommandGroup {
 	id: Id;
