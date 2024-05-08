e27885b45886ba4ee060d24c879d9a9ce22b5c31
TheKodeToad <TheKodeToad@proton.me>
2024-05-08 23:37:22 +0100

    Janky purge :3

--- src/common/discord_colors.ts
+++ src/common/discord_colors.ts
@@ -5,4 +5,5 @@ const fuchsia = 0xEB459E;
 const red = 0xED4245;
 const white = 0xFFFFFF;
 const black = 0x000000;
+
 export const Colors = { blurple, green, yellow, fuchsia, red, white, black };
---
+++ src/plugin/moderation/command/purge.ts
@@ -0,0 +1,71 @@
+import { AnyTextableGuildChannel, Permissions } from "oceanic.js";
+import { bot } from "../../..";
+import { OptionType, define_command } from "../../../core/types/command";
+
+export const purge_command = define_command({
+	id: "purge",
+	options: {
+		count: {
+			id: ["count", "c"],
+			type: OptionType.INTEGER,
+			position: 0,
+			required: true,
+		},
+		match: {
+			id: ["match", "m"],
+			type: OptionType.STRING,
+			position: 1,
+		},
+	},
+	async run(context, args) {
+		if (context.guild === null || context.member === null)
+			return;
+
+		const channel = (
+			context.guild.channels.get(context.channel_id)
+			?? context.guild.threads.get(context.channel_id)
+		) as AnyTextableGuildChannel | undefined;
+
+		if (channel === undefined)
+			return;
+
+		if (!channel.permissionsOf(context.member).has(Permissions.MANAGE_MESSAGES))
+			return;
+
+		let purged = 0;
+
+		const iter = bot.rest.channels.getMessagesIterator(context.channel_id, {
+			limit: args.count, before: context.message?.id
+		});
+
+		for await (const messages of iter) {
+			let stop = false;
+
+			const to_delete: string[] = [];
+			const two_weeks_ago = Date.now() - (1000 * 60 * 60 * 24 * 14);
+
+			for (const message of messages) {
+				if (message.createdAt.getTime() < two_weeks_ago) {
+					stop = true;
+					break;
+				}
+
+				if (args.match !== null && !message.content.includes(args.match))
+					continue;
+
+				to_delete.push(message.id);
+			}
+
+			await channel.deleteMessages(to_delete);
+			purged += to_delete.length;
+
+			if (stop)
+				break;
+		}
+
+		if (purged === 0)
+			await context.respond(":x: No messages were purged!");
+		else
+			await context.respond(`:white_check_mark: Purged ${purged} messages!`);
+	},
+});
