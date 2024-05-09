ce982c9f1ac579a46d4b72c0d7a4415a9060c659
TheKodeToad <TheKodeToad@proton.me>
2024-05-09 15:02:10 +0100

    Add GUILD_MEMBERS intent

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { DISCORD_TOKEN } from "./config";
 export const bot = new Client({
 	auth: `Bot ${DISCORD_TOKEN}`,
 	gateway: {
-		intents: Constants.AllNonPrivilegedIntents | Constants.Intents.MESSAGE_CONTENT
+		intents: Constants.AllNonPrivilegedIntents | Constants.Intents.MESSAGE_CONTENT | Constants.Intents.GUILD_MEMBERS
 	},
 	allowedMentions: {},
 });
