8e4ef7bfffd3a2c70c94be7111cae93611fb211c
TheKodeToad <TheKodeToad@proton.me>
2025-04-22 19:13:12 +0100

    Better guild cards

--- frontend/src/route/servers.tsx
+++ frontend/src/route/servers.tsx
@@ -1,21 +1,38 @@
+import { A } from "@solidjs/router";
 import { createResource, For, Show } from "solid-js";
 import { getGuilds, GuildResponse } from "../client";
 import { LoginGate } from "../component/LoginGate";
 import { account } from "../state/account";
 
-export const Servers = () => <LoginGate><ServersComponent /></LoginGate>;
+export const Servers = () => <LoginGate><GuildsComponent /></LoginGate>;
 
-function ServersComponent() {
+function GuildsComponent() {
 	const [guilds] = createResource(() => account() !== null ? getGuilds(account()!.token) : undefined);
 
 	return (
-		<div class="content">
+		<div class="content mainContent">
 			<h1>Servers</h1>
 			<Show when={!(guilds() === undefined || "error" in guilds()!)}>
-				<For each={(guilds() as GuildResponse[])!}>
-					{guild => guild.name + "\n"}
-				</For>
+				<div class={"guilds"}>
+					<For each={guilds() as GuildResponse[]}>
+						{guild => <GuildCard {...guild} />}
+					</For>
+				</div>
 			</Show>
 		</div>
+	);
+}
+
+function GuildCard({ name, iconHash, id }: { name: string; iconHash: string | null; id: string; }) {
+	const icon =
+		iconHash !== null
+			? `https://cdn.discordapp.com/icons/${id}/${iconHash}.png?size=256`
+			: "https://cdn.discordapp.com/embed/avatars/0.png";
+
+	return (
+		<A href={`/servers/${id}`} class="guildCard">
+			<img src={icon} />
+			<span class="guildCard-title">{name}</span>
+		</A>
 	);
 }
