482ca13f954616bccea67257476948d3c32302f0
TheKodeToad <TheKodeToad@proton.me>
2025-05-05 01:08:54 +0100

    Enable compression and etag

--- backend/src/http/route/frontend.ts
+++ backend/src/http/route/frontend.ts
@@ -1,6 +1,8 @@
 import { serveStatic } from "@hono/node-server/serve-static";
 import { randomBytes } from "crypto";
 import { Hono } from "hono";
+import { compress } from "hono/compress";
+import { etag } from "hono/etag";
 import { html, raw } from "hono/html";
 import { APP_DESCRIPTION, APP_INVITE_PERMISSIONS, APP_LIBRARIES_LINK, APP_NAME, APP_SOURCE_CODE } from "../../brand.ts";
 import { CLIENT_ID, REDIRECT_URI } from "../../environment.ts";
@@ -8,7 +10,7 @@ import { CLIENT_ID, REDIRECT_URI } from "../../environment.ts";
 const router = new Hono;
 
 // TODO: disturbing
-router.use("/static/*", serveStatic({ root: "../frontend" })); // yea
+router.use("/static/*", compress(), etag(), serveStatic({ root: "../frontend" })); // yea
 
 const constants = JSON.stringify({
 	CLIENT_ID,
