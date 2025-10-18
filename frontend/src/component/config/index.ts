6d75c48f146327f6f2f397c47b5ab68092451e33
TheKodeToad <TheKodeToad@proton.me>
2025-10-18 12:23:57 +0100

    Cleanup TOML language definition

--- frontend/src/component/config/index.ts
+++ frontend/src/component/config/index.ts
@@ -2,32 +2,21 @@ import { indentLess, insertTab } from "@codemirror/commands";
 import { continuedIndent, foldNodeProp, indentNodeProp, indentUnit, LRLanguage } from "@codemirror/language";
 import { Command, EditorView, keymap } from "@codemirror/view";
 import { gruvboxDark } from "@fsegurai/codemirror-theme-gruvbox-dark";
-import { SyntaxNode } from "@lezer/common";
 import { basicSetup } from "codemirror";
 import { parser } from "lezer-toml";
-
-
-function foldExceptFirst(node: SyntaxNode) {
-	if (node.firstChild === null || node.lastChild === null)
-		return null;
-
-	//const firstLine = state.doc.lineAt(first.from);
-	//const secondLine = state.doc.lineAt(second.from);
-	return { from: node.firstChild.to, to: node.lastChild.to };
-}
-
-function foldCharOffset(fromOffset: number, toOffset: number) {
-	return (node: SyntaxNode) => ({ from: node.from + fromOffset, to: node.to + toOffset });
-}
 
 const toml = LRLanguage.define({
 	parser: parser.configure({
 		props: [
 			indentNodeProp.add({ Array: continuedIndent({ except: /^\s*]/ }) }),
 			foldNodeProp.add({
-				Table: foldExceptFirst,
-				ArrayTable: foldExceptFirst,
-				Array: foldCharOffset(1, -1),
+				"Table ArrayTable": node => {
+					if (node.firstChild === null || node.lastChild === null)
+						return null;
+
+					return { from: node.firstChild.to, to: node.lastChild.to };
+				},
+				Array: node => ({ from: node.from + 1, to: node.to - 1 })
 			})
 		]
 	}),
