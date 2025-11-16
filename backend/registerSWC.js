09545954cb78fea13ad3c2909c0ce50e0b6f9510
TheKodeToad <TheKodeToad@proton.me>
2025-11-16 23:28:57 +0000

    Get rid of registerSWC.js

--- backend/registerSWC.js
+++
@@ -1,5 +0,0 @@
-import { register } from "node:module";
-import path from "node:path";
-import { pathToFileURL } from "node:url";
-
-register("@swc-node/register/esm", pathToFileURL(path.join(import.meta.dirname, "node_modules")));
