c0cb5dc311498bd5e8ba85141b0739b0c7fc97db
TheKodeToad <TheKodeToad@proton.me>
2025-11-24 21:29:26 +0000

    Set trackUpdates for tag command

--- backend/src/plugin/tags/command/tag.ts
+++ backend/src/plugin/tags/command/tag.ts
@@ -4,12 +4,13 @@ import { defineCommand } from "#plugin/core/public/extensionPoints.ts";
 import { permissionsGuard } from "#plugin/core/public/helper/commandGuards.ts";
 import { icons } from "#plugin/core/public/icons.ts";
 import { MAX_TAG_NAME_LENGTH } from "#plugin/tags/constants.ts";
-import { autocompleteTags } from "#plugin/tags/helper/command.ts";
+import { autocompleteTags } from "#plugin/tags/helper/autocompletion.ts";
 import { tagsConfigStore } from "#plugin/tags/index.ts";
 import { getTag } from "#plugin/tags/storage/tags.ts";
 
 export default defineCommand({
 	name: ["tag", "tagsend", "sendtag"],
+	trackUpdates: true,
 
 	options: {
 		name: {
