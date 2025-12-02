803e8493e3f12532eb24c485f21e3c627d863dce
TheKodeToad <TheKodeToad@proton.me>
2025-12-02 22:06:25 +0000

    Fix escapeMarkdown doc comment rendering

--- backend/src/common/discord/markdown.ts
+++ backend/src/common/discord/markdown.ts
@@ -8,6 +8,7 @@ const ZWSP = "\u200B";
  * Escape all characters used for markdown formatting.
  * Mainly used whenever a username is being displayed.
  * Make sure that the field supports formatting - a common mistake is to use in embed titles which do not have foramtting applied!
+ *
  * !! Do not use as a replacement for allowedMentions or SUPRESS_EMBEDS !!
  */
 export function escapeMarkdown(input: string): string {
